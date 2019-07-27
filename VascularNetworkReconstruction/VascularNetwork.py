f163073ff14a65fcfe5caa270d88261942208503
Junhong Shen <48073240+sjunhongshen@users.noreply.github.com>
2019-07-27 19:42:15 -0400

    Update VascularNetwork.py

--- VascularNetworkReconstruction/VascularNetwork.py
+++ VascularNetworkReconstruction/VascularNetwork.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class VascularNetwork():
-    def __init__(self, root, leaves, inflow, r_init, f_init, p_init):
+    def __init__(self, root, leaves, r_init, f_init, p_init):
         self.tree = nx.Graph()
         self.node_count = 0
         self.add_branching_point(root, p_init)
         self.leaves= range(1, len(leaves) + 1)
         for leaf_loc in leaves:
             self.add_branching_point(leaf_loc)
-        self.k = None
+        self.k = 1
         self.c = 3
         self.mu = 2.8
         self.alpha = 1
@@ -19,18 +19,19 @@ class VascularNetwork():
         self.p_0 = p_init
 
     def add_branching_point(self, loc, pres=None):
-        self.tree.add_node(self.node_count, loc=loc, pressure=pres, HS=None)
+        self.tree.add_node(self.node_count, loc=loc, pressure=pres, HS=None, relaxed=False)
         self.node_count += 1
         return self.node_count - 1
 
     def add_vessel(self, node, neighbor_node, radius=None, flow=None):
-        radius = self.r_0 if radius == None else radius
-        flow = self.f_0 if flow == None else flow
-        self.tree.add_edge(node, neighbor_node, radius=radius, flow=flow, length=np.linalg.norm(np.array(self.tree.nodes[node]['loc']) - np.array(self.tree.nodes[neighbor_node]['loc'])))
+        r = self.r_0 if radius == None else radius
+        f = self.f_0 if flow == None else flow
+        dis = np.linalg.norm(np.array(self.tree.nodes[node]['loc']) - np.array(self.tree.nodes[neighbor_node]['loc']))
+        self.tree.add_edge(node, neighbor_node, radius=r, flow=f, length=dis)
 
     def merge(self, node1, node2):
         for n in self.tree.neighbors(node2):
-            if node2 != n:
+            if node1 != n:
                 self.add_vessel(node1, n, self.tree[node2][n]['radius'], self.tree[node2][n]['flow'])
         self.tree.remove_node(node2)
 
@@ -58,11 +59,12 @@ class VascularNetwork():
 
     def reconnect(self):
         for leaf in self.leaves:
-            min_length = np.linalg.norm(np.array(self.tree.nodes[0]['loc']) - np.array(self.tree.nodes[leaf]['loc']))
+            min_dis = np.linalg.norm(np.array(self.tree.nodes[0]['loc']) - np.array(self.tree.nodes[leaf]['loc']))
             nearest_node = 0
             for node in self.tree.nodes:
-                if node not in self.leaves and np.linalg.norm(np.array(self.tree.nodes[node]['loc']) - np.array(self.tree.nodes[leaf]['loc'])) < min_length:
-                    min_length = np.linalg.norm(np.array(self.tree.nodes[node]['loc']) - np.array(self.tree.nodes[leaf]['loc']))
+                dis = np.linalg.norm(np.array(self.tree.nodes[node]['loc']) - np.array(self.tree.nodes[leaf]['loc']))
+                if node not in self.leaves and dis < min_dis:
+                    min_dis = dis
                     nearest_node = node
             if nearest_node == 0:
                 self.add_vessel(nearest_node, leaf, self.r_0, self.f_0)
@@ -72,9 +74,9 @@ class VascularNetwork():
     def move_node(self, node, loc_new):
         self.tree.nodes[node]['loc'] = loc_new
         for n in self.tree.neighbors(node):
-            self.tree[node1][n]['length'] = np.linalg.norm(self.tree.nodes[node]['loc'] - self.tree.nodes[n]['loc'])
+            self.tree[node][n]['length'] = np.linalg.norm(np.array(self.tree.nodes[node]['loc']) - np.array(self.tree.nodes[n]['loc']))
 
-    def update_radius(self, edge, r_new):
+    def update_radius_and_flow(self, edge, r_new):
         node1, node2 = edge
         self.tree[node1][node2]['radius'] = r_new
         self.tree[node1][node2]['flow'] = self.k * (r_new ** self.c)
@@ -94,19 +96,24 @@ class VascularNetwork():
 
     def update_strahler_order(self):
         for n in self.tree.nodes:        
-            self.tree.nodes[n]['HS'] = 1 if n.degree == 0 else 0
-        count_none = self.node_count
+            self.tree.nodes[n]['HS'] = 1 if self.tree.degree[n] == 1 else 0
+        count_no_label = self.node_count
         cur_order = 1
-        while count_none != 0:
+        while count_no_label != 0:
             for node in self.tree.nodes:
+                print(node)
+                if self.tree.nodes[node]['HS'] != 0:
+                    continue
                 neighbor_orders = np.array([self.tree.nodes[n]['HS'] for n in self.tree.neighbors(node)])
-                if 0 in neighbor_orders:
+                print(neighbor_orders)
+                if node == 0 and np.count_nonzero(neighbor_orders == 0) > 0:
+                    continue
+                if node != 0 and np.count_nonzero(neighbor_orders == 0) > 1:
                     continue
                 max_order = np.max(neighbor_orders)
                 max_count = np.count_nonzero(neighbor_orders == max_order)
                 self.tree.nodes[node]['HS'] = max_order if max_count == 1 else max_order + 1
-            count_none = np.count_nonzero(np.array([self.tree.nodes[n]['HS'] for n in self.tree.nodes]) == 0)
-
+            count_no_label = np.count_nonzero(np.array([self.tree.nodes[n]['HS'] for n in self.tree.nodes]) == 0)
 
     def get_conductance(self, edge):
         node1, node2 = edge
@@ -125,21 +132,24 @@ class VascularNetwork():
         return (4 * self.mu * self.tree[node1][node2]['flow']) / (np.pi * self.tree[node1][node2]['radius'] ** 3)
 
 if __name__ == '__main__':
-    VN = VascularNetwork((0,0,0),[(0,1,0),(1,0,0)],3,0.1,1,2)
-    VN.add_branching_point((0,0,1))
-    VN.add_branching_point((1, 2, 1))
+    VN = VascularNetwork((0,0),[(0,4),(4,0)],0.1,3,2)
+    VN.add_branching_point((1,1))
+    VN.add_branching_point((1, 2))
     VN.add_vessel(0, 1)
     VN.add_vessel(0, 2)
     VN.add_vessel(0, 3)
-    VN.add_branching_point((0,1,1))
+    VN.add_branching_point((2,1))
     VN.add_vessel(4, 3)
     VN.add_vessel(5, 3)
-    VN.split(3, (1,1,1), [4])
-
-    def f(x):
-        return True if x[0] > 4 or x[1] > 4 else False
     
-    VN.prune(f)
+    VN.split(3, (1.5,1.5), [4,5])
+    VN.split(0, (2,2), [1])
+    VN.split(0, (3,1), [2])
+    VN.move_node(1, (1, 4))  
+    VN.merge(3, 6)  
+    VN.prune(1)
+    VN.reconnect()
+    """
     VN.split(0, (0,1,0), [1])
     VN.split(0, (1,8,9), [2])
     VN.split(0, (1,1,1), [3])
@@ -148,5 +158,13 @@ if __name__ == '__main__':
     VN.tree.remove_edge(9,3)
     VN.reconnect()
     plt.subplot(121)
-    nx.draw(VN.tree, with_labels=True, font_weight='bold')
+    #nx.draw(VN.tree, with_labels=True, font_weight='bold')
+    #plt.show()
+    """
+    locs = nx.get_node_attributes(VN.tree,'loc')
+    nx.draw(VN.tree, locs)
+    label1 = nx.get_node_attributes(VN.tree, 'HS')
+    label2 = nx.get_edge_attributes(VN.tree, 'length')
+    nx.draw_networkx_labels(VN.tree, locs, label1)
+    nx.draw_networkx_edge_labels(VN.tree, locs, edge_labels=label2)
     plt.show()
