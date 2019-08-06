e1b59d8604b58c650e7d60f5045173752b8da1b0
Abdul Hannan Faruqi <abdul.hannan@zhcet.ac.in>
2019-08-07 02:46:31 +0530

    Add files via upload

---
+++ VascularNetworkReconstruction/Simulated Annealing/SimAnneal.h
@@ -0,0 +1,8 @@
+double SimAnneal(double, double, double);
+double CostCal(double, double, double);
+int move(double *, double *, double *, double, double, double, double);
+int maxmin (double *, double *, double *);
+double Diff(double, double, double);
+
+double x[1000], y[1000], z[1000], d1[6], d2[6];
+int imax;
